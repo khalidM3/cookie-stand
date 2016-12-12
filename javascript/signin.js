864a4b01d7f61c56ad1e562a64c011356c02cdca
Khalid Mohamud <kmohamud1@outlook.com>
2016-12-12 08:55:57 -0800

    commitiing full cookie stand for submition

--- javascript/signin.js
+++ javascript/signin.js
@@ -1,22 +1,23 @@
 'use strict';
-
 var signOwner = document.getElementById('signownerin');
 signOwner.addEventListener('submit',signIn);
 var takerToPage = document.getElementById('div');
 
-function signIn(event) {
-  event.preventDefault();
+  function signIn(event) {
+    event.preventDefault();
 
-  var a = event.target.firstname.value;
-  var b = event.target.lastname.value;
-  var c = event.target.password.value;
-  var d = event.target.codename.value;
-  if (a === 'pat' && b === 'patterson' && c === 'patcook' && d === 'owner') {
-    takerToPage.innerHTML = '<a href="sales.html">The owner page</a>';
-    console.log(a);
-    console.log(b);
-    console.log();
-
-  }
-
-}
+    var a = event.target.firstname.value;
+    var b = event.target.lastname.value;
+    var c = event.target.password.value;
+    var d = event.target.codename.value;
+      if (a == 'pat' && b == 'patterson' && c == 'patcook' && d == 'owner') {
+        takerToPage.innerHTML = '<button><a href="sales.html">The owner page</a></button>';
+        a = null;
+        b = null;
+        c = null;
+        d = null;
+      } else {
+        alert('Sorry, your information is incorrect!');
+        takerToPage.innerHTML = '<button><a href="index.html">Go to home page</a></button>'
+      }
+    }
